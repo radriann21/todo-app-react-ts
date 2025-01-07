4428cb484966fbd64673a35bd5cbd712bc7ad925
radriann21 <adrianrangelargu@gmail.com>
2025-01-07 19:28:21 -0400

    initialize project with Vite, React, TypeScript, and Tailwind CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="font-primaryRegular text-3xl text-red-600">hello world</h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          'bright-blue': 'hsl(220, 98%, 61%)',
+        },
+        light: {
+          'very-light-gray': 'hsl(0, 0%, 98%)',
+          'very-light-grayish-blue': 'hsl(236, 33%, 92%)',
+          'light-grayish-blue': 'hsl(233, 11%, 84%)',
+          'dark-grayish-blue': 'hsl(236, 9%, 61%)',
+          'very-dark-grayish-blue': 'hsl(235, 19%, 35%)'
+        },
+        dark: {
+          'very-dark-blue': 'hsl(235, 21%, 11%)',
+          'very-dark-desaturated-blue': 'hsl(235, 24%, 19%)',
+          'light-grayish-blue': 'hsl(234, 39%, 85%)',
+          'light-grayish-blue-hover': 'hsl(236, 33%, 92%)',
+          'dark-grayish-blue': 'hsl(234, 11%, 52%)',
+          'very-dark-grayish-blue': 'hsl(233, 14%, 35%)',
+          'very-dark-grayish-blue-2': 'hsl(237, 14%, 26%)'
+        }
+      },
+      fontFamily: {
+        'primaryRegular': ['Regular'],
+        'primarySemibold': ['Semibold']
+      }
+    },
+  },
+  plugins: [],
+}
+
