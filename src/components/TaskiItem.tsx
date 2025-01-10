82c42506a07ffd0825a0a597a588edaf1f42bdac
radriann21 <adrianrangelargu@gmail.com>
2025-01-09 22:14:27 -0400

    enhance task item checkbox styling for improved UI experience

--- src/components/TaskiItem.tsx
+++ src/components/TaskiItem.tsx
@@ -17,7 +17,12 @@ export const TaskItem = ({ task }: TaskItemProps) => {
   return (
     <li className="py-4 px-4 text-left w-full text-light-very-dark-grayish-blue flex items-center justify-between cursor-pointer border-b-2 dark:border-b-dark-very-dark-grayish-blue-2">
       <div className="flex items-center space-x-8">
-        <input onChange={handleToggleCompleted} type="checkbox" checked={task.completed} />
+        <input
+          className="appearance-none h-4 w-4 border-2 border-light-very-dark-grayish-blue rounded-full checked:bg-blue-500 checked:border-transparent focus:outline-none transition duration-200 cursor-pointer"
+          onChange={handleToggleCompleted}
+          type="checkbox"
+          checked={task.completed}
+        />
         <span className={`dark:text-dark-light-grayish-blue ${task.completed === false ? null : 'line-through text-light-light-grayish-blue'}`}>{task.name}</span>
       </div>
       <button onClick={handleDeleteTask}>
